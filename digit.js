8a1d429fe226afb21e15537900bd2ac9084a823f
Mohammad Khubaib <120662666+khubaib004@users.noreply.github.com>
2023-03-08 19:48:09 +0600

    digit

---
+++ digit.js
@@ -0,0 +1,24 @@
+let digit=prompt("Enter the digit : ");
+
+if (digit==0)
+    document.log("zero");
+else if (digit==1)
+    document.log("one");
+else if (digit==2)
+    document.log("two");
+else if (digit==3)
+    document.log("three");
+else if (digit==4)
+    document.log("four");
+else if (digit==5)
+    document.log("five");
+else if (digit==6)
+    document.log("six");
+else if (digit==7)
+    document.log("seven");
+else if (digit==8)
+    document.log("eight");
+else if (digit==9)
+    document.log("nine");
+else
+    document.log("invalid number")
