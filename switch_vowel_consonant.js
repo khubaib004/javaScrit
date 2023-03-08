1896b219b2fbb92f515590e8fcc0690ccd02f0d0
Mohammad Khubaib <120662666+khubaib004@users.noreply.github.com>
2023-03-08 20:17:13 +0600

    using switch

---
+++ switch_vowel_consonant.js
@@ -0,0 +1,24 @@
+let letter = prompt("Enter the letter : ");
+
+letter = letter.toLowerCase();
+
+switch (letter) {
+  case "a":
+    console.log("VOWEl");
+    break;
+  case "e":
+    console.log("VOWEl");
+    break;
+  case "i":
+    console.log("VOWEl");
+    break;
+  case "o":
+    console.log("VOWEl");
+    break;
+  case "u":
+    console.log("VOWEL");
+    break;
+  default:
+    console.log("CONSONANT");
+    break;
+}
