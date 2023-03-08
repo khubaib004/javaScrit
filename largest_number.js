c6732c3d40743130c235bd4968ea84fca28a5d6a
Mohammad Khubaib <120662666+khubaib004@users.noreply.github.com>
2023-03-08 17:21:10 +0600

    largest number

---
+++ largest_number.js
@@ -0,0 +1,12 @@
+let a = parseInt(prompt("Enter the first number : "));
+let b = parseInt(prompt("Enter the first number : "));
+let c = parseInt(prompt("Enter the first number : "));
+
+if (a>b && a>c)
+    console.log("A is a largest number");
+
+else if (b>a && b>c)
+    console.log("B is a largest number");
+
+else
+    console.log("C is a lagest number")
