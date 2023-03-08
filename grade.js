66b837bcfe615a1ee22862457d9256a584d5a5f5
Mohammad Khubaib <120662666+khubaib004@users.noreply.github.com>
2023-03-08 17:07:42 +0600

    grade by relational operator

---
+++ grade.js
@@ -0,0 +1,16 @@
+let mark=parseInt(prompt("Enter the marks : "));
+
+if (mark>=90)
+    console.log("A+");
+else if (mark>=80)
+    console.log("A");
+else if (mark>=70)
+    console.log("A-");
+else if (mark>=60)
+    console.log("B");
+else if (mark>=50)
+    console.log("C");
+else if (mark>=40)
+    console.log("D");
+else if (mark<=33)
+    console.log("F");
