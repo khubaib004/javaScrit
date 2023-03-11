3267ea2a72d1e11c0e3d2210d033b3a57820ed31
Mohammad Khubaib <120662666+khubaib004@users.noreply.github.com>
2023-03-11 19:06:34 +0600

    how to create & call a function

---
+++ function/create_call.js
@@ -0,0 +1,12 @@
+// create a function 
+function name(){
+    let number = Number (prompt("Enter the number : "));
+
+    number= number ** 2;
+
+    document.write("Square is : " + number + "<br>");
+}
+// calling a function
+
+name();
+name();
