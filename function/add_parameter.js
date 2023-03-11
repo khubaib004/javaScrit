7b0ded18ee8b068f880baae1e6003f5281962021
Mohammad Khubaib <120662666+khubaib004@users.noreply.github.com>
2023-03-11 19:10:14 +0600

    how to add parameter

---
+++ function/add_parameter.js
@@ -0,0 +1,11 @@
+// create a function 
+
+function name(number){    // add parameter
+    number= number ** 2;
+
+    document.write("Square is : " + number + "<br>");
+}
+// calling a function
+
+name(2);
+name(8);
