44e443589f679bc6dfe77ad73d466acd6256f754
Mohammad Khubaib <120662666+khubaib004@users.noreply.github.com>
2023-03-12 12:21:24 +0600

    sample

---
+++ Array/first.js
@@ -0,0 +1,13 @@
+let name=new Array(5);
+
+name[0]="Al-Habib"
+name[1]="Khubaib"
+name[2]="Rupom"
+name[3]="Mahid"
+name[4]="Nahid"
+
+console.log("No-1 : " + name[0]);
+console.log("No-2 : " + name[1]);
+console.log("No-3 : " + name[2]);
+console.log("No-4 : " + name[3]);
+console.log("No-5 : " + name[4]);
