3c3f28c5b4ea449ff9eef81524e5365df4991cc7
Mohammad Khubaib <120662666+khubaib004@users.noreply.github.com>
2023-04-02 02:37:45 -0400

    if else condition

---
+++ boolean-value.js
@@ -0,0 +1,11 @@
+let a = 0,
+   b = 0;
+for (let i = -5; i <= 10; i++) {
+   if (a === 4) {
+      b++;
+      console.log("this is b " + b);
+   } else {
+      a++;
+      console.log("this is a " + a);
+   }
+}
